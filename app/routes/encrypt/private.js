bb283ee00be7f323ac3a060af02564fb131f5d90
Felix Mattick <felix.mattick@gmail.com>
2018-08-22 18:44:18 -0400

    Add more elements

--- app/routes/encrypt/private.js
+++ app/routes/encrypt/private.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import cryptico from 'npm:cryptico';
 
 export default Route.extend({
+  spinner: service('spinner'),
+  actions: {
+    go() {
+      this.get('spinner').show('spinner');
+      encrypt('Test', 'pass');
+    }
+  }
 });
+
+function encrypt(string, pass) {
+  var bits       = 1024;
+  var privateKey = cryptico.generateRSAKey (pass, bits);
+  var publicKey  = cryptico.publicKeyString(privateKey);
+}
