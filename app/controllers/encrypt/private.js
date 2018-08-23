231820eab5c3cb444b549a758190f153b769b4b8
Felix Mattick <felix.mattick@gmail.com>
2018-08-22 20:26:56 -0400

    Get encryption to (mostly) work

--- app/controllers/encrypt/private.js
+++ app/controllers/encrypt/private.js
@@ -2,12 +2,21 @@ import Controller from '@ember/controller';
 import cryptico from 'npm:cryptico';
 
 export default Controller.extend({
+  loader:  true,
   actions: {
     go: function() {
+      //this.set('loader', false);
+
       var bits       = 1024;
       var privateKey = cryptico.generateRSAKey (this.get('pass'), bits);
       var publicKey  = cryptico.publicKeyString(privateKey);
-      this.set('key', publicKey);
+      this.set('key', 'Public key: ' + publicKey);
+
+      var plainText       = this.get('string');
+      var encryptedString = cryptico.encrypt(plainText, privateKey).cipher;
+      this.set('output', 'Encrypted string: ' + encryptedString)
+
+      //this.set('loader', true);
     }
   }
 });
