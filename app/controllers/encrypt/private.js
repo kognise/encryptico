122c10db01bc0a977d5d8eea45f0750a9ef34599
Felix Mattick <felix.mattick@gmail.com>
2018-08-23 08:50:28 -0400

    I'm making a big change

--- app/controllers/encrypt/private.js
+++ app/controllers/encrypt/private.js
@@ -1,22 +1,17 @@
 import Controller from '@ember/controller';
-import cryptico from 'npm:cryptico';
+import keypair from 'npm:keypair' ;
+import NodeRSA from 'npm:node-rsa';
 
 export default Controller.extend({
-  loader:  true,
+  loaderHidden:  true,
   actions: {
-    go: function() {
-      //this.set('loader', false);
-
-      var bits       = 1024;
-      var privateKey = cryptico.generateRSAKey (this.get('pass'), bits);
-      var publicKey  = cryptico.publicKeyString(privateKey);
-      this.set('key', 'Public key: ' + publicKey);
-
-      var plainText       = this.get('string');
-      var encryptedString = cryptico.encrypt(plainText, privateKey).cipher;
-      this.set('output', 'Encrypted string: ' + encryptedString)
-
-      //this.set('loader', true);
+    success: function() {},
+    error:   function() {},
+    go:      function() {
+      var pair = keypair();
+      this.set('_key', pair.public);
+      this.set('key' , pair.public.replace('\n', '<br/>'));
+      const key = new NodeRSA();
     }
   }
 });
