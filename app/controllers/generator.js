19f9553263e2c7ed573146073f203f2063fa9688
Felix Mattick <felix.mattick@gmail.com>
2018-08-28 14:26:43 -0400

    Working generator

--- app/controllers/generator.js
+++ app/controllers/generator.js
@@ -1,4 +1,36 @@
-import Controller from '@ember/controller';
+import  Controller  from '@ember/controller';
+import { htmlSafe } from '@ember/template'  ;
+
+import keypair from 'npm:keypair' ;
+import nl2br   from 'npm:nl2br'   ;
+import rsa     from 'npm:node-rsa';
 
 export default Controller.extend({
+  color:        '',
+  outputClass:  'hidden',
+  loaderHidden: true,
+  actions: {
+    copied: function() {
+      this.set('color', 'green');
+      setTimeout(function(self) { self.set('color', '') }, 1000, this);
+    },
+    copyError: function() {
+      this.set('color', 'red');
+      setTimeout(function(self) { self.set('color', '') }, 1000, this);
+    },
+    go: function() {
+      this.set('loaderHidden', false);
+
+      setTimeout(function(self) {
+        var kp = keypair();
+        self.set('_public', kp.public);
+        self.set('public', new htmlSafe(nl2br(kp.public)));
+        self.set('_private', kp.private);
+        self.set('private', new htmlSafe(nl2br(kp.private)));
+
+        self.set('outputClass', '');
+        self.set('loaderHidden', true);
+      }, 50, this);
+    }
+  }
 });
