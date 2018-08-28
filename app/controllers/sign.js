d6fdfe0e722a4eb0939c29a79d62b852b9828ff6
Felix Mattick <felix.mattick@gmail.com>
2018-08-27 20:57:32 -0400

    Add better loader, start signing and verification

--- app/controllers/sign.js
+++ app/controllers/sign.js
@@ -1,4 +1,40 @@
-import Controller from '@ember/controller';
+import  Controller  from '@ember/controller';
+import { htmlSafe } from '@ember/template'  ;
+
+import keypair from 'npm:keypair' ;
+import nl2br   from 'npm:nl2br'   ;
+import rsa     from 'npm:node-rsa';
 
 export default Controller.extend({
+  color:        '',
+  outputClass:  'hidden',
+  loaderHidden: true,
+  actions: {
+    copied: function() {
+      this.set('color', 'green');
+      setTimeout(function(self) { self.set('color', '') }, 1000, this);
+    },
+    copyError: function() {
+      this.set('color', 'red');
+      setTimeout(function(self) { self.set('color', '') }, 1000, this);
+    },
+    go: function() {
+      this.set('loaderHidden', false);
+
+      setTimeout(function(self) {
+        var kp = keypair();
+        var priv = new rsa();
+        priv.importKey(kp.private, 'pkcs1-private-pem');
+
+        self.set('_key', kp.public);
+        self.set('key' , new htmlSafe(nl2br(kp.public)));
+
+        var signature = priv.sign(self.get('string'), 'base64');
+        self.set('signature', signature);
+
+        self.set('outputClass', '');
+        self.set('loaderHidden', true);
+      }, 50, this);
+    }
+  }
 });
